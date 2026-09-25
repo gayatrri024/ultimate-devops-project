63067c858487e65a6457b50b46e22e26eb6810dc
Gayu <gayatrishinde@akiyam.com>
2026-09-25 20:51:30 +0530

    infra: add Terraform and Argo CD configuration

---
+++ Terraform/main.tf
@@ -0,0 +1,23 @@
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name        = "ultimate-devops-vpc"
+    Environment = "dev"
+    ManagedBy   = "Terraform"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name      = "ultimate-devops-public-subnet"
+    ManagedBy = "Terraform"
+  }
+}
---
+++ Terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "aws_region" {
+  description = "AWS region used for the infrastructure plan"
+  type        = string
+  default     = "ap-south-1"
+}
